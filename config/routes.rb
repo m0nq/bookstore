06a7320d3b42a1828f14da16fae74fff8bbf35c0
Monk <forgetmenaught@gmail.com>
2016-09-27 14:17:06 -0700

    finish generating basic controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,46 @@
 Rails.application.routes.draw do
+  get 'publishers/new'
+
+  get 'publishers/create'
+
+  get 'publishers/update'
+
+  get 'publishers/edit'
+
+  get 'publishers/destroy'
+
+  get 'publishers/index'
+
+  get 'publishers/show'
+
+  get 'authors/new'
+
+  get 'authors/create'
+
+  get 'authors/update'
+
+  get 'authors/edit'
+
+  get 'authors/destroy'
+
+  get 'authors/index'
+
+  get 'authors/show'
+
+  get 'categories/new'
+
+  get 'categories/create'
+
+  get 'categories/update'
+
+  get 'categories/edit'
+
+  get 'categories/destroy'
+
+  get 'categories/index'
+
+  get 'categories/show'
+
   get 'books/new'
 
   get 'books/create'
