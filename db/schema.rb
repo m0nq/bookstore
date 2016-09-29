10ddf451af47ce1b69d622ed1038ac0f0597484d
Monk <forgetmenaught@gmail.com>
2016-09-29 15:17:22 -0700

    add coverpath column to books table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160929203918) do
+ActiveRecord::Schema.define(version: 20160929220301) do
 
   create_table "authors", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "first_name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20160929203918) do
     t.integer  "pages"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+    t.string   "coverpath"
   end
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
