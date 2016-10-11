8e24ca557ecef77cc312ba1a72a208fd40015b2d
Monk <forgetmenaught@gmail.com>
2016-10-11 13:22:29 -0700

    update gemfile and db for postgres

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,6 +28,3 @@ Category.create(name: "PHP")
 Category.create(name: "MySQL")
 Category.create(name: "JavaScript")
 Category.create(name: "HTML & CSS")
-
-# Books should have a title, isbn, year, price, buy (link), excerpt, format, pages (number), coverpath (link)
-book = Book.create(title: "Ajax on Rails", isbn: "0596527446", year: "2007", price: "$0.18", buy: "https://www.amazon.com/gp/offer-listing/0596527446/ref=tmm_pap_new_olp_sr?ie=UTF8&condition=new&qid=1475703104&sr=8-1", excerpt: "Learn to build dynamic, interactive web applications using the two most important approaches to web development today: Ajax and the phenomenally efficient Ruby on Rails platform. This book teaches intermediate to advanced web developers how to use both Ajax and Rails to quickly build high-performance, scalable applications without being overwhelmed with thousands of lines of JavaScript code. More than just recipes, you also get a thorough, low-level understanding of what's happening under the hood.", format: "Paperback", pages: 352, coverpath: "https://images-na.ssl-images-amazon.com/images/I/515tCdqABsL._SX379_BO1,204,203,200_.jpg")
